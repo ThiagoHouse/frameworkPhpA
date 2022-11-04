c99e3baae8ee884fd91fbf7949bc44f406eef9ef
thyago-carlos <thyago-carlos@hotmail.com>
2022-11-04 16:42:42 -0300

    Wip implementa rotas

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,8 @@
 <?php
 
 $route[] = ['/', 'HomeController@index'];
+$route[] = ['/posts', 'PostsController@index'];
+$route[] = ['/posts/{id}/show', 'PostsController@show'];
+
 
 return $route;
--- core/Route.php
+++ core/Route.php
@@ -9,9 +9,20 @@ class Route
     public function __construct(array $routes)
     {
         $this->routes = $routes;
+        $this->run();
     }
 
-    public function getUrl(){
+    private function getUrl(){
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+
+    private function run()
+    {
+        $url = $this->getUrl();
+        $urlArray = explode('/', $url);
+
+        foreach ($this->routes as $route){
+            $urlArray = explode('/', $route[0]);
+        }
     }
 }
